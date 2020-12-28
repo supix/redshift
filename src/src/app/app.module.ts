5b520e5915b317eac15802cd4f91247dc3dfd32d
Marcello Esposito <esposito.marce@gmail.com>
2020-12-28 16:36:08 +0100

    Shifts Component available

--- src/src/app/app.module.ts
+++ src/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ShiftsComponent } from './components/shifts/shifts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShiftsComponent
   ],
   imports: [
     BrowserModule,
