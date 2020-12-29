4e4301e9337b69e9b9431b09406899a9c4c234ce
Marcello Esposito <esposito.marce@gmail.com>
2020-12-29 23:48:49 +0100

    Shift table has graphical improvements
    Headings and shifts have horizontal alignment.
    Headings have a lighter font.

--- src/src/app/services/shifts/shifts.fake.service.ts
+++ src/src/app/services/shifts/shifts.fake.service.ts
@@ -55,7 +55,47 @@ const fakeData: ManShift[] = [
         null,
         'http://url-turno.test.net'
       ),
-    ])
+    ]),
+  new ManShift(
+    'Antonio Bianchi',
+    'Interno',
+    [
+      new Presenze(
+        new Date(2020, 11, 9),
+        'G',
+        'Giornata piena',
+        null,
+        'http://url-turno.test.net'
+      ),
+      new Presenze(
+        new Date(2020, 11, 10),
+        'G',
+        'Giornata piena',
+        null,
+        'http://url-turno.test.net'
+      ),
+      new Presenze(
+        new Date(2020, 11, 11),
+        'K',
+        'Kubernetes',
+        null,
+        'http://url-turno.test.net'
+      ),
+      new Presenze(
+        new Date(2020, 11, 15),
+        'K',
+        'Kubernetes',
+        null,
+        'http://url-turno.test.net'
+      ),
+      new Presenze(
+        new Date(2020, 11, 16),
+        'K',
+        'Kubernetes',
+        null,
+        'http://url-turno.test.net'
+      ),
+    ]),
 ];
 
 
