a624d510f83b3fdac047eaf3862b257e94cf40a6
Marcello Esposito <esposito.marce@gmail.com>
2020-12-30 17:53:26 +0100

    There is an empty navbar component.

--- src/src/app/components/navbar/navbar.component.ts
+++ src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUserNinja } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  faUserNinja = faUserNinja;
   constructor() { }
 
   ngOnInit(): void {
