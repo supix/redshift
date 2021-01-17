eb965e5a1bab5c7a27896d64008fb80762832013
Marcello Esposito <esposito.marce@gmail.com>
2021-01-17 22:48:25 +0100

    Shifts daily resume appears
    It contains fake data. The cells are not aligned, yet. The rendering is a little bit ugly. Some ideas needed.

---
+++ src/src/app/components/shift-daily-resume/ShiftCounts.ts
@@ -0,0 +1,7 @@
+export class ShiftCounts {
+    constructor(
+        public readonly code: string,
+        public readonly description: string,
+        public readonly count: number
+    ) {}
+}
--- src/src/app/components/shift-daily-resume/shift-daily-resume.component.ts
+++ src/src/app/components/shift-daily-resume/shift-daily-resume.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ShiftsDataService } from 'src/app/services/shifts-data/shifts-data.service';
+import { DayInfo } from '../shifts/DayInfo';
+import { ShiftCounts } from './ShiftCounts';
 
 @Component({
   selector: 'app-shift-daily-resume',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shift-daily-resume.component.css']
 })
 export class ShiftDailyResumeComponent implements OnInit {
+  daysToShow: DayInfo[];
 
-  constructor() { }
+  constructor(
+    private readonly shiftsDataService: ShiftsDataService
+  ) { }
 
   ngOnInit(): void {
+    this.init();
   }
 
+  private init(): void {
+    this.daysToShow = this.shiftsDataService.daysToShow;
+  }
+
+  public getCounts(d: Date): ShiftCounts[] {
+    return [
+      new ShiftCounts('M', 'Mattina', 5),
+      new ShiftCounts('P', 'Pomeriggio', 4),
+      new ShiftCounts('N', 'Notte', 2),
+    ]
+  }
 }
